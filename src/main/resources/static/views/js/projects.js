6214bc68f0caaea8f8af4faa13aae7f1a5c1337c
-v <wtrzos@gmail.com>
2018-09-14 18:42:38 +0200

    js highlightig in calendar

---
+++ src/main/resources/static/views/js/projects.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    var events = $('.event');
+    events.each(function (index, element) {
+        var evdate = $(element).data('eventdate');
+        var classdate = '.' + evdate;
+
+        var checkboxDate = $('#cb' + evdate);
+        checkboxDate.attr('disabled', 'true');
+        checkboxDate.parent().on('mouseenter', function () {
+            $(classdate).addClass('highlight');
+        });
+        checkboxDate.parent().on('mouseleave', function () {
+            $(classdate).removeClass('highlight');
+        });
+        $(element).on('mouseenter', function () {
+            $(classdate).addClass('highlight');
+        });
+        $(element).on('mouseleave', function () {
+            $(classdate).removeClass('highlight');
+        });
+
+    })
+
+
+});
